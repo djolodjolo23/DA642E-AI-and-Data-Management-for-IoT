1243acfa603bc503bd9e3a0e7d496e5c4757bb5d
Djordje Dimitrov <110050027+djolodjolo23@users.noreply.github.com>
2025-02-13 16:51:53 +0100

    Update install_all_deps.sh

--- LAB_2/install_all_deps.sh
+++ LAB_2/install_all_deps.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Create a global virtual environment if it doesn’t exist
 if [ ! -d "global_env" ]; then
     python3 -m venv global_env
 fi
 
-# Activate the virtual environment
 source global_env/bin/activate
 
 echo "Installing dependencies from all subfolders into global_env..."
 
-# Find all virtual environments and install their dependencies into global_env
 for venv in $(find . -type f -name "pyvenv.cfg"); do
     venv_dir=$(dirname "$venv")
     echo "Found virtual environment in: $venv_dir"
